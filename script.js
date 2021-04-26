c5e83cf8adf24a118a326041c1bcecba1e72dded
Mark Sirany <marksirany@me.com>
2021-04-26 17:14:55 -0500

    50 percent done

---
+++ script.js
@@ -0,0 +1,14 @@
+//1. Print odds 1-20 - Using a loop write code that will console.log all of the odd values from 1 up to 20.
+for (var x=0; x<=20; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+
+}
+
+//Print the sequence - Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+var x = ["4", "2.5", "1", "-0.5", "-2", "-3.5"];
+console.log(x);
